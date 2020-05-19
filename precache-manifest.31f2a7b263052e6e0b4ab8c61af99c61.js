89749b3561abba70c50f291d0a625fab9254484d
Alex Kovacs <kovacs-a@ulster.ac.uk>
2020-05-19 19:45:23 +0100

    Updates

---
+++ precache-manifest.31f2a7b263052e6e0b4ab8c61af99c61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8986a8c38db7b43f5bd077d05ceed05",
+    "url": "/reactcovidtracker/index.html"
+  },
+  {
+    "revision": "4a4f04fcaaf81452d976",
+    "url": "/reactcovidtracker/static/css/main.9ca08ade.chunk.css"
+  },
+  {
+    "revision": "bba3a393a277b465a416",
+    "url": "/reactcovidtracker/static/js/2.873151bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactcovidtracker/static/js/2.873151bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a4f04fcaaf81452d976",
+    "url": "/reactcovidtracker/static/js/main.da16fe22.chunk.js"
+  },
+  {
+    "revision": "b9ae326ef816e8c8392b",
+    "url": "/reactcovidtracker/static/js/runtime-main.469212d5.js"
+  }
+]);
